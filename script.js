d308b5a0f37001966395b28e913cc81198b14574
dannygallows <43937866+dannygallows@users.noreply.github.com>
2024-11-05 21:45:49 +0100

    Adjust back to the flexbox solution, respond correctly to the amount of boxes

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ function createGrid(size) {
 
     const gridDiv = document.querySelector(".grid");
 
+    const boxsize = 500 / size;
+
+
     // grid solution by https://stackoverflow.com/questions/57550082/creating-a-16x16-grid-using-javascript
-    gridDiv.style.setProperty('--grid-rows', size);
-    gridDiv.style.setProperty('--grid-cols', size);  
+    // gridDiv.style.setProperty('--grid-rows', size);
+    // gridDiv.style.setProperty('--grid-cols', size);  
 
 
     for (let i = 0; i < size * size; i++) {
         const gridSquare = document.createElement("div");
         gridSquare.classList.add("grid-square");
+        gridSquare.style.flex = `0 0 ${boxsize}px`
+        gridSquare.style.height = `${boxsize}px`
         gridDiv.appendChild(gridSquare);    
     }
 }
