84df4e254acd6a130204c1d4500cba909e12d2a8
dannygallows <43937866+dannygallows@users.noreply.github.com>
2024-11-05 22:32:14 +0100

    Add Function to reset the grid

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+const DEFAULT_GRID_SIZE = 16;
+
 function createGrid(size) {
 
     const gridDiv = document.querySelector(".grid");
+
+    gridDiv.innerHTML = "";
 
     const boxsize = 500 / size;
 
@@ -13,16 +17,31 @@ function createGrid(size) {
     }
 }
 
-createGrid(16);
+createGrid(DEFAULT_GRID_SIZE);
+selectSquares();
 
-const gridSquares = document.querySelectorAll(".grid-square");
 
-gridSquares.forEach (square => {
-    square.addEventListener("mouseover", () => square.style.backgroundColor = "red")
+const newGridButton = document.querySelector("#new-grid");
+
+newGridButton.addEventListener("click", () => {
+    createGrid(DEFAULT_GRID_SIZE);
+    selectSquares()
 })
 
+const resetGridButton = document.querySelector("#reset-grid");
 
+resetGridButton.addEventListener("click", () => {
+    createGrid(16);
+    selectSquares()
+})
 
+function selectSquares() {
+    const gridSquares = document.querySelectorAll(".grid-square");
+
+    gridSquares.forEach (square => {
+    square.addEventListener("mouseover", () => square.style.backgroundColor = "red")
+})
+}
 
 
 
